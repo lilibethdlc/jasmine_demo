4578167789e70a463ec0ef56b9e891268c2058f2
Lilibeth De La Cruz <lilibethdlc@gmail.com>
2015-03-16 00:47:14 -0400

    Karma run test

---
+++ karma_run_test/hello_world_spec.js
@@ -0,0 +1,9 @@
+describe('helloWorld', function(){
+  it('returns "Hello World!"', function(){
+    expect(helloWorld()).toBe("Hello World!");
+  });
+
+  // it('fails', function(){
+  //   expect(helloWorld()).toBe("NOT Hello World!");
+  // });
+});
---
+++ karma_run_test/karma.conf.js
@@ -0,0 +1,20 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      '*.js',
+      'tests/**/*spec.js'
+    ],
+    exclude: [
+    ],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['PhantomJS'],
+
+    // Leave running or not
+    autoWatch: true,
+    singleRun: true
+  });
+};
