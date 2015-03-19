7ec96cb6acf77b608ecc9f04068fe55fffd2133f
Lilibeth De La Cruz <lilibethdlc@gmail.com>
2015-03-19 10:47:53 -0400

    Examples taken from the Jasmine doc

---
+++ setup_teardown.js
@@ -0,0 +1,17 @@
+describe("Setup and Teardown", function() {
+  beforeEach(function() {
+    //...
+  });
+
+  afterEach(function() {
+    //...
+  });
+
+  beforeAll(function() {
+    //...
+  });
+
+  afterAll(function() {
+    //...
+  });
+});
